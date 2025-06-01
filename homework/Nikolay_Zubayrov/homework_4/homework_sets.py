29ecbc01354a933cb82d9d42cdbf30309ead348e
https://github.com/Niko1218-create <zubayrovn@mail.ru>
2025-06-01 15:02:31 +0300

    homework 4 done

---
+++ homework/Nikolay_Zubayrov/homework_4/homework_sets.py
@@ -0,0 +1,25 @@
+my_dict = {
+    'tuple': (1, 2, 3, 4, 5),
+    'list': [4, 'text', 7, 3.5, 'world'],
+    'dict': {'name': 'Ivan', 'age': 20, 'city': 'Moskow', 'car': 'Volvo', 'gender': 'man'},
+    'set': {2, 8, 'hello', 3.7, 11}
+
+}
+
+my_dict['tuple'][-1]
+print(my_dict['tuple'][-1])
+
+my_dict['list'].append('Hello')
+print(my_dict['list'])
+my_dict['list'].pop(2)
+print(my_dict['list'])
+
+my_dict['dict']['i am a tuple'] = 'yes'
+print(my_dict['dict'])
+my_dict['dict'].pop('name')
+print(my_dict['dict'])
+
+my_dict['set'].add(0)
+print(my_dict['set'])
+my_dict['set'].remove(11)
+print(my_dict['set'])
