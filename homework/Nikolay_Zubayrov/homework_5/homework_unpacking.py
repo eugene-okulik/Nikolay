348e26f615a1ea07fe7e57898fdfec87ecf67e0b
https://github.com/Niko1218-create <zubayrovn@mail.ru>
2025-06-10 21:21:38 +0300

    homework_5 updated

--- homework/Nikolay_Zubayrov/homework_5/homework_unpacking.py
+++ homework/Nikolay_Zubayrov/homework_5/homework_unpacking.py
@@ -3,20 +3,21 @@ person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 name, last_name, city, phone, country = person
 print(name, last_name, city, phone, country)
 
-
 # Задание 2
 result1 = 'результат операции: 42'
-my_list = result1.split()
-print(int(my_list[2]) + 10)
+result_index = result1.index(':')
+new_result = int(result1[result_index + 1:])
+print(new_result + 10)
 
 result2 = 'результат операции: 514'
-my_list = result2.split()
-print(int(my_list[2]) + 10)
+result_index = result2.index(':')
+new_result = int(result2[result_index + 1:])
+print(new_result + 10)
 
 result3 = 'результат работы программы: 9'
-my_list = result3.split()
-print(int(my_list[3]) + 10)
-
+result_index = result3.index(':')
+new_result = int(result3[result_index + 1:])
+print(new_result + 10)
 
 # Задание 3
 students = ['Ivanov', 'Petrov', 'Sidorov']
