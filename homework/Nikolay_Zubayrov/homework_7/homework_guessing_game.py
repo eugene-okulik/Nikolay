10e4b590f532438dffd85c12f7867d303a502163
https://github.com/Niko1218-create <zubayrovn@mail.ru>
2025-06-22 14:59:40 +0300

    homework_7_done

---
+++ homework/Nikolay_Zubayrov/homework_7/homework_guessing_game.py
@@ -0,0 +1,16 @@
+# задание 1
+number = 5
+while True:
+    enter_num = int(input('Enter num: '))
+    if enter_num != number:
+        print('попробуйте снова')
+        # continue
+    elif enter_num == number:
+        print('Поздравляю! Вы угадали!')
+        break
+
+print('Досвидания!')
+
+
+
+
