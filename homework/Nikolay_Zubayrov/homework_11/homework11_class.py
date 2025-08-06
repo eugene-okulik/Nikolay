88dd799220766dc4ef312e0d45254d53dc315786
https://github.com/Niko1218-create <zubayrovn@mail.ru>
2025-08-06 19:49:47 +0300

    homework_11 update

--- homework/Nikolay_Zubayrov/homework_11/homework11_class.py
+++ homework/Nikolay_Zubayrov/homework_11/homework11_class.py
@@ -1,51 +1,51 @@
 class Book:
-    page_material = 'материал: бумага'
+    page_material = 'бумага'
     text = True
-
-
-class Pushkin(Book):
-    book_title = 'Название: Лукоморье'
-    author = 'Автор: Пушкин'
-    number_of_pages = 'страниц: 50'
-    ISBN = 101
-
-
-class Dostoevsky(Book):
-    book_title = 'Название: Идиот'
-    author = 'Автор: Достоевский'
-    number_of_pages = 'страниц: 500'
-    ISBN = 101
 
     def __init__(self, reserved):
         self.reserved = reserved
 
 
-class Griboyedov(Book):
-    book_title = 'Название: Горе от ума'
-    author = 'Автор: Грибоедов'
-    number_of_pages = 'страниц: 400'
+class Pushkin(Book):
+    book_title = 'Лукоморье'
+    author = 'Пушкин'
+    number_of_pages = 50
     ISBN = 101
 
 
-pushkin = Pushkin()
-dostoevsky = Dostoevsky('зарезервирована')
-griboyedov = Griboyedov()
+class Dostoevsky(Book):
+    book_title = 'Идиот'
+    author = 'Достоевский'
+    number_of_pages = 500
+    ISBN = 102
 
-print(pushkin.book_title)
-print(pushkin.author)
-print(pushkin.number_of_pages)
-print(pushkin.page_material)
+
+class Griboyedov(Book):
+    book_title = 'Горе от ума'
+    author = 'Грибоедов'
+    number_of_pages = 400
+    ISBN = 103
+
+
+pushkin = Pushkin('не зарезервирована')
+dostoevsky = Dostoevsky('зарезервирована')
+griboyedov = Griboyedov('не зарезервирована')
+
+print('Название:', pushkin.book_title)
+print('Автор: ', pushkin.author)
+print('страниц: ', pushkin.number_of_pages)
+print('материал: ', pushkin.page_material)
 print()
-print(dostoevsky.book_title)
-print(dostoevsky.author)
-print(dostoevsky.number_of_pages)
-print(dostoevsky.page_material)
+print('Название: ', dostoevsky.book_title)
+print('Автор: ', dostoevsky.author)
+print('страниц: ', dostoevsky.number_of_pages)
+print('материал: ', dostoevsky.page_material)
 print(dostoevsky.reserved)
 print()
-print(griboyedov.book_title)
-print(griboyedov.author)
-print(griboyedov.number_of_pages)
-print(griboyedov.page_material)
+print('Название: ', griboyedov.book_title)
+print('Автор: ', griboyedov.author)
+print('страниц: ', griboyedov.number_of_pages)
+print('материал: ', griboyedov.page_material)
 print()
 
 
@@ -54,51 +54,49 @@ class SchoolTextbooks(Book):
 
 
 class Mathematics(SchoolTextbooks):
-    name = 'Название: математика'
-    lesson = 'Предмет: алгебра'
-    group = 'класс: 6'
-    author = 'Автор: Зыкин'
-    number_of_pages = 'страниц: 50'
-
-    def __init__(self, reserved):
-        self.reserved = reserved
+    book_title = 'математика'
+    lesson = 'алгебра'
+    group = '6б'
+    author = 'Зыкин'
+    number_of_pages = 50
 
 
 class Story(SchoolTextbooks):
-    name = 'Название: история'
-    lesson = 'Предмет: история мира'
-    group = 'класс: 6'
-    author = 'автор: Иванов'
-    number_of_pages = 'страниц: 507'
+    book_title = 'история'
+    lesson = 'история мира'
+    group = '8a'
+    author = 'Иванов'
+    number_of_pages = 507
 
 
 class Geography(SchoolTextbooks):
-    name = 'Название: география'
-    lesson = 'Предмет: география мира'
-    group = 'класс: 8'
-    author = 'автор: Петров'
-    number_of_pages = 'страниц: 550'
+    book_title = 'география'
+    lesson = 'география мира'
+    group = '8б'
+    author = 'Петров'
+    number_of_pages = 550
 
 
 mathematics = Mathematics('зарезервирована')
-story = Story()
-geography = Geography()
 
-print(mathematics.name)
-print(mathematics.author)
-print(mathematics.number_of_pages)
-print(mathematics.lesson)
-print(mathematics.group)
+story = Story('не зарезервирована')
+geography = Geography('не зарезервирована')
+
+print('Название: ', mathematics.book_title)
+print('Автор: ', mathematics.author)
+print('страниц: ', mathematics.number_of_pages)
+print('Предмет: ', mathematics.lesson)
+print('класс: ', mathematics.group)
 print(mathematics.reserved)
 print()
-print(story.name)
-print(story.author)
-print(story.number_of_pages)
-print(story.lesson)
-print(story.group)
+print('Название: ', story.book_title)
+print('Автор: ', story.author)
+print('страниц: ', story.number_of_pages)
+print('Предмет: ', story.lesson)
+print('класс: ', story.group)
 print()
-print(geography.name)
-print(geography.author)
-print(geography.number_of_pages)
-print(geography.lesson)
-print(geography.group)
+print('Название: ', geography.book_title)
+print('Автор: ', geography.author)
+print('страниц: ', geography.number_of_pages)
+print('Предмет: ', geography.lesson)
+print('класс: ', geography.group)
