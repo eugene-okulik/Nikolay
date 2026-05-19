f3fb3f60e03ee208e1290c52524aa28b388cdfee
Nikolay Zubayrov <zubayrovn@mail.ru>
2026-05-19 21:05:15 +0300

    Домашняя работа 29 добавлена

---
+++ homework/Nikolay_Zubayrov/homework_29/task_2.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import Page, expect, BrowserContext
+
+
+def test_tabs_element_enabled(page: Page, context: BrowserContext):
+    page.goto('https://www.qa-practice.com/elements/new_tab/button')
+    link = page.get_by_role('link', name='Click')
+    with context.expect_page() as new_page_event:
+        link.click()
+
+    new_page = new_page_event.value
+    result = new_page.locator('#result-text')
+    expect(result).to_have_text('I am a new page in a new tab')
+    click_button = page.get_by_role('link', name='Click')
+    expect(click_button).to_be_enabled()
+
